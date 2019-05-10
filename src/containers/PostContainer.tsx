31b0019c6f1f94d00b060e47fae9aa7dc9abf537
hyunalee <dlgusdk419@naver.com>
2019-05-10 11:23:53 +0900

    refactor: remove console

--- src/containers/PostContainer.tsx
+++ src/containers/PostContainer.tsx
@@ -19,7 +19,6 @@ const PostContainer = () => {
   function getPostsDefault() {
     fetch('https://jsonplaceholder.typicode.com/todos')
       .then((response) => {
-        console.log(response)
         response.json().then((result) => {
           setPosts(result);
         });
